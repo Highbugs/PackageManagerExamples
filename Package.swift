392328f261fd8375806195d5ad913d7d88e681f0
Tiago Pereira <highbugs@gmail.com>
2020-06-18 11:53:18 +0200

    commit

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     ],
     targets: [
         .target(
-          name: "SwiftExample",
+          name: "LibraryName",
            dependencies: [], 
            path: "./SwiftExample"
            )
