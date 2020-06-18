8238ee063f44a6f7970811204fdc41d4dad7f90b
Tiago Pereira <highbugs@gmail.com>
2020-06-18 15:41:48 +0200

    add objc files

---
+++ SwiftExample/ThatClass.h
@@ -0,0 +1,16 @@
+//
+//  ThatClass.h
+//  ExampleName
+//
+//  Created by Tiago Pereira on 18/06/2020.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ThatClass : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ SwiftExample/ThatClass.m
@@ -0,0 +1,12 @@
+//
+//  ThatClass.m
+//  ExampleName
+//
+//  Created by Tiago Pereira on 18/06/2020.
+//
+
+#import "ThatClass.h"
+
+@implementation ThatClass
+
+@end
